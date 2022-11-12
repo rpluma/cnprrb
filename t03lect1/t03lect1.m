334037299c8db649bdbefb1f8f9299fa3c5d34cb
rpluma <99877837+rpluma@users.noreply.github.com>
2022-11-12 12:06:40 +0100

    t03.3 hist e histogram
    Dibuja el histograma para distintos valores de muestras
    100 500 1000 ))[comando hist

---
+++ t03lect1/t03lect1.m
@@ -0,0 +1,25 @@
+% #8 The Gaussian Distribution 2D
+
+clear;
+mu = zeros(2, 1);
+Sigma=[3 0;0 2]
+SigmaInv = inv(Sigma);
+x=[1;0]; 
+Delta2 = (((x-mu)')*SigmaInv)*(x-mu) % escalar que define la elipse
+clear;
+
+mu = zeros(2, 1);
+Sigma=[3 1;1 4]
+SigmaInv = inv(Sigma);
+x=[1;0]; 
+Delta2 = (((x-mu)')*SigmaInv)*(x-mu) % escalar que define la elipse
+clear;
+
+%muestreo(100, 0, 1);
+
+%close all;
+%hist(randn(100));
+
+%close all;
+%hist(randn(1000));
+histogram(randn(100));
