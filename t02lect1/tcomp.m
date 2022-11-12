2b958ea3d60b0fa5dcf69818ff393398a7551516
rpluma <99877837+rpluma@users.noreply.github.com>
2022-11-12 07:47:58 +0100

    Ejercicios t02lect1 salvo último
    Depuración código de función tcomp
    Scripts penúltima página de la ppt

--- t02lect1/tcomp.m
+++ t02lect1/tcomp.m
@@ -1,12 +1,12 @@
 function [p] = tcomp(p1, p2)
     % ----------------- 1 Extraer los componentes de cada pose
-    x1 = p1[1];
-    y1 = p1[2];
-    t1 = p1[3];
+    x1 = p1(1);
+    y1 = p1(2);
+    t1 = p1(3);
 
-    x2 = p2[1];
-    y2 = p2[2];
-    t2 = p1[3];
+    x2 = p2(1);
+    y2 = p2(2);
+    t2 = p2(3);
 
     % ----------------- 2 Componer los ángulos y ajustar a [-Pi,Pi]
     t3 = t1 + t2;
@@ -19,5 +19,5 @@ function [p] = tcomp(p1, p2)
     % ---------------- 3 Componern las posiciones
     c = cos(t1);
     s = sin(t1);
-    p = [x1 + x2*c - y2*s, y1 + x2*s + y2*c, t3]
+    p = [x1 + x2*c - y2*s, y1 + x2*s + y2*c, t3];
 end
