0a8e90e5f36b3f595cd4e254e604ee05714080a8
rpluma <99877837+rpluma@users.noreply.github.com>
2022-11-12 08:52:55 +0100

    Lab1.1 pose_comp vectorial

---
+++ p01lab01/pose_comp.m
@@ -0,0 +1,22 @@
+function p=pose_comp(p1, p2)
+    % This function computes the composition of two 3x1 poses p1,p2
+    % p is the resultant pose
+    
+    % obtenemos theta1, su seno y su coseno
+    t1=p1(3); 
+    ct1=cos(t1); 
+    st1=sin(t1);
+
+    % definimos una matriz de rotación R para hacer la composición
+    R=[ct1 st1 0; -st1 ct1 0; 0 0 1];
+
+    % calculamos la pose como p1 + (p2'*R)'
+    p = p1 + (p2' * R)';
+
+    % restringimos el ángulo al intervalo [-pi, pi]
+    if (p(3) > pi)
+        p(3) = p(3) - 2*pi;
+    elseif(p(3) < -pi)
+        p(3) = p(3) + 2*pi;
+    end
+end
