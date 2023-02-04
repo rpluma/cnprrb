05825ddec1d70ae3bfb8d0b00a1bfc714e74eac3
rpluma <99877837+rpluma@users.noreply.github.com>
2023-02-04 08:30:51 +0100

    lab08.3 funcionando bien

--- l08a/lab08.3.lua
+++ l08a/lab08.3.lua
@@ -21,6 +21,10 @@ function coroutineMain()
 	deltatime=5
 	side = 0.8
 	iter = 1
+	zVac = 0.07 -- Altura de la ventosa
+	zCub = 0.10 -- Altura del cubo
+	xDif = 0.8  -- Movimiento en el eje x
+	back = false -- Regreso a posición inicial
 
 	-- Obtener los manejadores
 	hRob = sim.getObject("/IRB140")
@@ -28,6 +32,11 @@ function coroutineMain()
 	hDum = sim.getObject("/Dummy")
 	hTip = sim.getObject("/IRB140/tip")
 	hTar = sim.getObject("/IRB140/target")
+	hVac = sim.getObject('/IRB140/BaxterVacuumCup')
+	hCb0 = sim.getObject('/Cuboid[0]')
+	hCb1 = sim.getObject('/Cuboid[1]')
+	hCb2 = sim.getObject('/Cuboid[2]')
+	aVac = sim.getObjectAlias(hVac, 4)
 
 	-- Posiciones iniciales
 	pRob = sim.getObjectPosition(hRob, -1)
@@ -35,44 +44,73 @@ function coroutineMain()
 	iRes = sim.setObjectPosition(hDum, -1, pNew)
 	pDum = sim.getObjectPosition(hDum, -1)
 	pTar = sim.getObjectPosition(hTar, -1)
+	pLft = sim.getObjectPosition(hCb0, -1)
 
 
 	-- Inicialización de Rucking
-	--pTip = sim.getObjectPose(hTip, -1)
-	pTar = sim.getObjectPose(hTar, -1)
+	pTip = sim.getObjectPose(hTip, -1)
+	--pTar = sim.getObjectPose(hTar, -1)
 	maxVel = {0.4, 0.4, 0.4, 0.4}
 	maxAcc = {0.1, 0.1, 0.1, 0.1}
 	maxJrk = {0.1, 0.1, 0.1, 0.1}
-	--tarPos = sim.getObjectPose(hTip, -1)
-	tarPos = sim.getObjectPose(hTar, -1)
 	local function moveToPose_callback(curPose, curVel, curAcc, hTar)
 	 	sim.setObjectPose(hTar,-1,curPose)
 	end
 
 	-- Bucle principal
 	while (true) do
-		curPos = sim.getObjectPose(hTip, -1)
-		--if curPos[1]==tarPos[1] and curPos[2]==tarPos[2] and curPos[3]==tarPos[3] then
-			print(iter)		
-			if iter%4 == 1 then 
-				pNew[1] = pDum[1] 
-				pNew[2] = pDum[2]
-			elseif iter%4 == 2 then
-				pNew[1] = pDum[1]-side
-				pNew[2] = pDum[2];
-			elseif iter%4 == 3 then
-				pNew[1] = pDum[1]-side
-				pNew[2] = pDum[2]-side;
-			elseif iter%4 == 0 then
-				pNew[1] = pDum[1]
-				pNew[2] = pDum[2]-side;
-			end
-			iter = iter + 1
-			tarPos = {pNew[1], pNew[2], pNew[3], pTar[4], pTar[5], pTar[6], pTar[7]}
+		-- curPos = sim.getObjectPose(hTar, -1)
+		if back then
+			pOrg = {pLft[1]-xDif, pLft[2], pLft[3]}
+			pDst = {pLft[1],      pLft[2], pLft[3]}
+		else
+			pOrg = {pLft[1],      pLft[2], pLft[3]}
+			pDst = {pLft[1]-xDif, pLft[2], pLft[3]}
+		end
+
+		for iCub = 3, 1, -1 do
+
+			-- Cálculos de movimientos
+			zOrg = (iCub * zCub) + zVac
+			zDst = ((4-iCub)*zCub) + zVac
+			zMax = math.max(zOrg, zDst)+0.1
+
+			print(iCub, zOrg, zDst)
+
+			-- Subir por encima de las verticales de los cubos
+			curPos = sim.getObjectPose(hTar, -1)
+			tarPos = {curPos[1], curPos[2], zMax, curPos[4], curPos[5], curPos[6], curPos[7]}
 			sim.moveToPose(-1,curPos,maxVel,maxAcc,maxJrk,tarPos,moveToPose_callback, hTar)
-			-- iRes = sim.setObjectPosition(hMan, -1, pNew)
-			-- sim.wait(deltatime)
-		--end
+			
+			-- Ir hacia el cubo de la izquierda 
+			curPos = sim.getObjectPose(hTar, -1)
+			tarPos = {pOrg[1], pOrg[2], curPos[3], curPos[4], curPos[5], curPos[6], curPos[7]}
+			sim.moveToPose(-1,curPos,maxVel,maxAcc,maxJrk,tarPos,moveToPose_callback, hTar)
+						
+			-- Bajar el brazo y recogerlo
+			curPos = sim.getObjectPose(hTar, -1)
+			tarPos = {pOrg[1], pOrg[2], zOrg, curPos[4], curPos[5], curPos[6], curPos[7]}
+			sim.moveToPose(-1,curPos,maxVel,maxAcc,maxJrk,tarPos,moveToPose_callback, hTar)
+			sim.setInt32Signal(aVac..'_active', 1)
+			
+			-- Subir el brazo
+			curPos = sim.getObjectPose(hTar, -1)
+			tarPos[3] = zMax
+			sim.moveToPose(-1,curPos,maxVel,maxAcc,maxJrk,tarPos,moveToPose_callback, hTar)
+			
+			-- Ir hacia la derecha
+			curPos = sim.getObjectPose(hTar, -1)
+			tarPos = {pDst[1], pDst[2], zMax, curPos[4], curPos[5], curPos[6], curPos[7]}
+			sim.moveToPose(-1,curPos,maxVel,maxAcc,maxJrk,tarPos,moveToPose_callback, hTar)
+			
+			-- Ir hacia abajo y soltarlo
+			curPos = sim.getObjectPose(hTar, -1)
+			tarPos = {pDst[1], pDst[2], zDst, curPos[4], curPos[5], curPos[6], curPos[7]}
+			sim.moveToPose(-1,curPos,maxVel,maxAcc,maxJrk,tarPos,moveToPose_callback, hTar)
+			sim.setInt32Signal(aVac..'_active', 0)			
+		end
+		back = not back
+
 	end
 end
 
